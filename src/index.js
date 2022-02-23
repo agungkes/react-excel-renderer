dec1b1df9d17839e2ecbd3c8af52699e9892cf15
M Rafli Fadillah <mraflif32@yahoo.com>
2022-02-23 17:31:30 +0700

    fix: change excel renderer to not use filereader

--- src/index.js
+++ src/index.js
@@ -35,31 +35,19 @@ export class OutTable extends Component {
     }
 }
 
-export function ExcelRenderer(file, callback) {
-    return new Promise(function(resolve, reject) {
-      var reader = new FileReader();
-      var rABS = !!reader.readAsBinaryString;
-      reader.onload = function(e) {
-        /* Parse data */
-        var bstr = e.target.result;
-        var wb = XLSX.read(bstr, { type: rABS ? "binary" : "array" });
+export async function ExcelRenderer(file, callback) {
+    var bstr = await file.arrayBuffer();
+    var wb = XLSX.read(bstr, { type: "array" });
 
-        /* Get first worksheet */
-        var wsname = wb.SheetNames[0];
-        var ws = wb.Sheets[wsname];
+    var wsname = wb.SheetNames[0];
+    var ws = wb.Sheets[wsname];
 
-        /* Convert array of arrays */
-        var json = XLSX.utils.sheet_to_json(ws, { header: 1 });
-        var cols = make_cols(ws["!ref"]);
+    var json = XLSX.utils.sheet_to_json(ws, { header: 1 });
+    var cols = make_cols(ws["!ref"]);
 
-        var data = { rows: json, cols: cols };
+    var data = { rows: json, cols: cols };
 
-        resolve(data);
-        return callback(null, data);
-      };
-      if (file && rABS) reader.readAsBinaryString(file);
-      else reader.readAsArrayBuffer(file);
-    });
+    return callback(null, data);
   }
 
   function make_cols(refstr) {
