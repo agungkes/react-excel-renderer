0b53e859b02d62bbfa651f6a9700959da6f23b5e
Agung Kurniawan E S <24893399+agungkes@users.noreply.github.com>
2022-03-01 15:55:01 +0700

    chore: change public

--- src/index.js
+++ src/index.js
@@ -1,60 +1,72 @@
-import React, { Component } from 'react';
-import * as XLSX from 'xlsx/xlsx.mjs'
+import React, { Component } from "react";
+import * as XLSX from "xlsx";
 
 export class OutTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-	constructor(props) {
-        super(props);
-        this.state = {
-
-        }
-    }
-
-	render() {
-        return (
-            <div className={this.props.className}>
-                <table className={this.props.tableClassName}  >
-                    <tbody>
-                        <tr>
-                            {this.props.withZeroColumn && !this.props.withoutRowNum && <th className={this.props.tableHeaderRowClass || ""}></th>}
-                            {
-                                this.props.columns.map((c) =>
-                                    <th key={c.key} className={c.key === -1 ? this.props.tableHeaderRowClass : ""}>{c.key === -1 ? "" : c.name}</th>
-                                )
-
-                            }
-                        </tr>
-                        {this.props.data.map((r,i) => <tr key={i}>
-                            {!this.props.withoutRowNum && <td key={i} className={this.props.tableHeaderRowClass}>{this.props.renderRowNum?this.props.renderRowNum(r,i):i}</td>}
-                            {this.props.columns.map(c => <td key={c.key}>{ r[c.key] }</td>)}
-                        </tr>)}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={this.props.className}>
+        <table className={this.props.tableClassName}>
+          <tbody>
+            <tr>
+              {this.props.withZeroColumn && !this.props.withoutRowNum && (
+                <th className={this.props.tableHeaderRowClass || ""}></th>
+              )}
+              {this.props.columns.map((c) => (
+                <th
+                  key={c.key}
+                  className={
+                    c.key === -1 ? this.props.tableHeaderRowClass : ""
+                  }>
+                  {c.key === -1 ? "" : c.name}
+                </th>
+              ))}
+            </tr>
+            {this.props.data.map((r, i) => (
+              <tr key={i}>
+                {!this.props.withoutRowNum && (
+                  <td key={i} className={this.props.tableHeaderRowClass}>
+                    {this.props.renderRowNum
+                      ? this.props.renderRowNum(r, i)
+                      : i}
+                  </td>
+                )}
+                {this.props.columns.map((c) => (
+                  <td key={c.key}>{r[c.key]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export async function ExcelRenderer(file, callback) {
-    var bstr = await file.arrayBuffer();
-    var wb = XLSX.read(bstr, { type: "array" });
+  var bstr = await file.arrayBuffer();
+  var wb = XLSX.read(bstr, { type: "array" });
 
-    var wsname = wb.SheetNames[0];
-    var ws = wb.Sheets[wsname];
+  var wsname = wb.SheetNames[0];
+  var ws = wb.Sheets[wsname];
 
-    var json = XLSX.utils.sheet_to_json(ws, { header: 1 });
-    var cols = make_cols(ws["!ref"]);
+  var json = XLSX.utils.sheet_to_json(ws, { header: 1 });
+  var cols = make_cols(ws["!ref"]);
 
-    var data = { rows: json, cols: cols };
+  var data = { rows: json, cols: cols };
 
-    return callback(null, data);
+  return callback(null, data);
+}
+
+function make_cols(refstr) {
+  var o = [],
+    C = XLSX.utils.decode_range(refstr).e.c + 1;
+  for (var i = 0; i < C; ++i) {
+    o[i] = { name: XLSX.utils.encode_col(i), key: i };
   }
-
-  function make_cols(refstr) {
-    var o = [],
-      C = XLSX.utils.decode_range(refstr).e.c + 1;
-    for (var i = 0; i < C; ++i) {
-      o[i] = { name: XLSX.utils.encode_col(i), key: i };
-    }
-    return o;
-  }
+  return o;
+}
